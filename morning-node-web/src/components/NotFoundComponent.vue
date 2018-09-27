1d0f3ceced0c261c2ded3761478b621a6eef1965
choxsu <2283546325@qq.com>
2018-09-27 13:43:33 +0800

    code to update ,,vue router mode to history

---
+++ morning-node-web/src/components/NotFoundComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      404 Not Found
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NotFoundComponent"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- morning-node-web/src/router/index.js
+++ morning-node-web/src/router/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import NotFoundComponent from '@/components/NotFoundComponent'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    {path: '/', name: 'HelloWorld', component: HelloWorld},
+    {path: '*', component: NotFoundComponent}
   ]
 })
