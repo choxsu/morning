2172b7f4bffaf51406b7a817e9df60f587870bab
choxsu <2283546325@qq.com>
2019-02-14 11:41:36 +0800

    code to update,Demo  二分查找  冒泡排序，  基于时间戳的分布式ID生成器

--- morning-service/src/main/java/com/syc/service/service/impl/BlogServiceImpl.java
+++ morning-service/src/main/java/com/syc/service/service/impl/BlogServiceImpl.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+
 @Service("blogService")
 public class BlogServiceImpl extends CommonService implements BlogService {
 
@@ -100,9 +101,61 @@ public class BlogServiceImpl extends CommonService implements BlogService {
         blog.setCategory("note");
         blogDao.insert(blog);
         //手动回滚DEMO,这种回滚是无需抛出异常，就可以回滚,调用了setRollbackOnly方法 记得return,不return的话 后面的业务也会被回滚
-        if (JFinalTxAop.setRollbackOnly()){
+        if (JFinalTxAop.setRollbackOnly()) {
             return Ret.fail("msg", "保存失败，数据被手动回滚！");
         }
         return Ret.ok("msg", "保存成功");
+    }
+
+    public static void main(String[] args) {
+        int[] a = {5, 2, 3, 4, 1};
+        int swap;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                //System.out.println("a[" + j + "]:" + a[j] + "|a[" + i + "]:" + a[i]);
+                if (a[j] > a[i]) {
+                    swap = a[i];
+                    a[i] = a[j];
+                    a[j] = swap;
+                }
+                //System.out.println(Arrays.toString(a));
+            }
+            //System.out.println("-----------------------------");
+        }
+        System.out.println(Arrays.toString(a));
+
+        int[] b = {5, 2, 3, 4, 1};
+        System.out.println(ef(b, 2));
+    }
+
+    /**
+     * 二分查找
+     java中有三种移位运算符
+     <<      :     左移运算符，num << 1,相当于num乘以2
+     >>      :     右移运算符，num >> 1,相当于num除以2
+     >>>    :     无符号右移，忽略符号位，空位都以0补齐,可以防止溢出
+     * @param a
+     * @param des
+     * @return
+     */
+    public static int ef(int[] a, int des) {
+        //定义初始最小、最大索引
+        int low = 0;
+        int high = a.length - 1;
+        //确保不会出现重复查找，越界
+        while (low <= high) {
+            //计算出中间索引值
+            int middle = (high + low) >>> 1;//防止溢出
+            if (des == a[middle]) {
+                return middle;
+                //判断下限
+            } else if (des < a[middle]) {
+                high = middle - 1;
+                //判断上限
+            } else {
+                low = middle + 1;
+            }
+        }
+        return -1;
     }
 }
