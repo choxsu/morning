5dc3715ade3017094d13fab56766c3d8ed37b5b8
choxsu <choxsu@163.com>
2021-03-04 10:35:38 +0800

    内容管理 控制器提交

---
+++ morning-api/src/main/java/com/syc/api/controller/article/ArticleApiController.java
@@ -0,0 +1,23 @@
+/*
+  Copyright © 2020 choxsu
+  https://choxsu.cn
+  All rights reserved.
+ */
+package com.syc.api.controller.article;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author choxsu
+ * @date 2021/3/4
+ * @describe
+ */
+@RestController
+@RequestMapping("/api/article")
+public class ArticleApiController {
+
+
+
+
+}
