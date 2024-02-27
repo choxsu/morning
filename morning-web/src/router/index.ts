5ba1f808629db1f149c8289318c2c9dad9d4dbec
xq.su <ycc_admin@163.com>
2024-02-27 09:21:49 +0800

    add feature

--- morning-web/src/router/index.ts
+++ morning-web/src/router/index.ts
@@ -1,21 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import FrontLayout from '@/layout/FrontLayout.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'Front',
+      component: FrontLayout,
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: () => import('@/views/home/HomeView.vue')
+        },
+        {
+          path: '/about',
+          name: 'about',
+          component: () => import('@/views/about/AboutView.vue')
+        }
+      ]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/login',
+      name: 'Login',
+      component: () => import('@/views/login/Login.vue')
     }
   ]
 })
